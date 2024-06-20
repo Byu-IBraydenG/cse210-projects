b6221bc48c4e5da9a130982d4cf71ab1ba34d74a
Byu-IBraydenG <dragon.grant03@gmail.com>
2024-06-20 09:05:23 -0600

    learning list

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Person p1 = new Person{};
+        p1._firstname = "Mary";
+        p1._lastname = "Smith";
+        p1._age = 25;
+
+
+        Person p2 = new Person{};
+        p2._firstname = "John";
+        p2._lastname = "Wakins";
+        p2._age = 30;
     }
 }
